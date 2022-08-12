aded292dc18599dc7063339250ca1c8db38c01aa
RomulRem666 <110928678+RomulRem666@users.noreply.github.com>
2022-08-12 11:06:00 +0300

    2 lesson

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 console.log(112);
 console.log("Roman");
+
+console.log(08);
+console.log("August");
+console.log( 'Добро '+'пожаловать '+' на курс' + ' Roman' );
+// alert(12.08.2022)
+// number
+//Infinity
+//NaN
+//BigInt
+//string
+//boolean
+//null
+//undefined
+//object
+//typeof
+
+document.getElementById("one").innerHTML = "Hello World"
