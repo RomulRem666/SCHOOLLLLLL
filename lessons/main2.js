db88c2ddebcf403cf1a9b8fb209b54d04f16f954
RomulRem666 <110928678+RomulRem666@users.noreply.github.com>
2022-08-26 17:45:07 +0300

    lesson 4

--- lessons/main2.js
+++ lessons/main2.js
@@ -1,9 +1,44 @@
-let myscore = prompt("когда")
-console.log(myscore)
+// let myscore = prompt("когда")
+// console.log(myscore)
 
-if (myscore >100) {
-   alert (myscore)
-    console.log(myscore)
-}else {
-    alert(myscore)
-}
+// if (myscore >100) {
+//    alert (myscore)
+//     console.log(myscore)
+// }else {
+//     alert(myscore)
+// }
+//Lesson 4
+10%3
+1
+2**12
+4096
+5<3
+false
+5>3 
+true
+'3'>5 
+false
+0 == false
+true
+5>0
+true
+0>1
+false
+'1' == 1
+true
+'1' === 1
+false
+15 != 14
+true
+14 != 14
+false
+'stroka' > 'krik'
+false
+'kot' < 'kot'
+false
+'kot' = 'kot'
+true
+'dragon' > 'dragons'
+false
+
+alert('yabloko' > 'Y')
