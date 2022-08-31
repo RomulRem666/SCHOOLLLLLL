70db0baff5d57407d5083df24f7ead115113ca7a
RomulRem666 <110928678+RomulRem666@users.noreply.github.com>
2022-08-31 11:28:53 +0300

    lesson5

--- lessons/main2.js
+++ lessons/main2.js
@@ -8,37 +8,49 @@
 //     alert(myscore)
 // }
 //Lesson 4
-10%3
-1
-2**12
-4096
-5<3
-false
-5>3 
-true
-'3'>5 
-false
-0 == false
-true
-5>0
-true
-0>1
-false
-'1' == 1
-true
-'1' === 1
-false
-15 != 14
-true
-14 != 14
-false
-'stroka' > 'krik'
-false
-'kot' < 'kot'
-false
-'kot' = 'kot'
-true
-'dragon' > 'dragons'
-false
+// 10%3
+// 1
+// 2**12
+// 4096
+// 5<3
+// false
+// 5>3 
+// true
+// '3'>5 
+// false
+// 0 == false
+// true
+// 5>0
+// true
+// 0>1
+// false
+// '1' == 1
+// true
+// '1' === 1
+// false
+// 15 != 14
+// true
+// 14 != 14
+// false
+// 'stroka' > 'krik'
+// false
+// 'kot' < 'kot'
+// false
+// 'kot' = 'kot'
+// true
+// 'dragon' > 'dragons'
+// false
 
-alert('yabloko' > 'Y')
+// alert('yabloko' > 'Y')
+for(let i = 2; i<=10; i=i+2){
+    alert(i)
+}
+let sum = 0;
+     for (let i = 0; i <= 100; i++) {
+         sum = sum + i;
+      }
+     alert(sum);
+
+for(let i = 10; i>=2; i=i-2){
+        alert(i)
+    }
