1f06266c185bfca46c693b909657d0cd16509559
RomulRem666 <110928678+RomulRem666@users.noreply.github.com>
2022-09-06 13:54:57 +0300

    lesson 6

--- lessons/main2.js
+++ lessons/main2.js
@@ -42,15 +42,58 @@
 // false
 
 // alert('yabloko' > 'Y')
-for(let i = 2; i<=10; i=i+2){
-    alert(i)
-}
-let sum = 0;
-     for (let i = 0; i <= 100; i++) {
-         sum = sum + i;
-      }
-     alert(sum);
+// for(let i = 2; i<=10; i=i+2){
+//     alert(i)
+// }
+// let sum = 0;
+//      for (let i = 0; i <= 100; i++) {
+//          sum = sum + i;
+//       }
+//      alert(sum);
 
-for(let i = 10; i>=2; i=i-2){
-        alert(i)
-    }
+// for(let i = 10; i>=2; i=i-2){
+//         alert(i)
+//     }
+
+// let amigo = 3*3
+// switch(amigo){
+//     case 3:
+//         alert("too much")
+//         break
+//     case 9:
+//         alert('good')
+//         break
+//     default:
+//         alert('nt')
+//         break
+// }
+
+// a ?? b
+
+// result = (a !== 0 && a!== undefined) ? a:b
+
+// let user
+// alert (user ?? "anonimus")
+
+// let user = null
+// let phone = null
+// let adres = 'ok'
+// alert (user ?? phone ?? adres ? 'sluchayno')
+
+
+let number = prompt("Введи число")
+
+switch (number){
+    case 0:
+        console.log('Ты ввел 0')
+        break
+    case 15:
+        console.log('Ты ввел 15')
+        break
+     case 26:
+        console.log('Ты ввел 26')
+        break
+    case 150:
+        console.log('Ты ввел 150')
+        break
+}
